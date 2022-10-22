6704b235a2ff9f98a1ebaab61b89fbb37c2971cf
wsz <305185235@qq.com>
2022-10-22 17:55:45 +0800

    打包压缩优化 首页加载速度10s->2s

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,20 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import commpressPlugin from 'vite-plugin-compression'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+      vue(),
+      commpressPlugin({
+        verbose: true, // 默认即可
+        disable: false, //开启压缩(不禁用)，默认即可
+        deleteOriginFile: false, //删除源文件
+        threshold: 10240, //压缩前最小文件大小
+        algorithm: 'gzip', //压缩算法
+        ext: '.gz' //文件类型
+      })
+  ],
   server: {
     open: true,
     port: 3000,
